6bc38f57a86021208c12085353f6b01592fe4a9f
Saul <saul.vega2@gmail.com>
2023-10-16 17:17:48 -0300

    paltav2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
+'use client';
+
 import Image from 'next/image'
 import { Bungee_Spice } from 'next/font/google'
+import React from 'react';
 const bs =  Bungee_Spice({
   subsets: ['latin'],
   weight: '400',
 })
 export default function Home() {
+  const [hide, setHide] = React.useState(true);
+
+  function handleClick() {
+    setHide(!hide);
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className={bs.className}>palta</h1>
-     <Image
-      className='rotate'
-      alt="imagen palta"
-      src="https://www.pngmart.com/files/15/Avocado-Half-PNG-Transparent.png"
-      width={600}
-      height={600}>
-      </Image>
+    <main>
+      <div className='palta-container'>
+        <h1 className={bs.className}>palta</h1>
+        <Image
+          id='palta'
+          className={hide ? 'hide' : 'rotate'}
+          alt='imagen palta'
+          src='https://www.pngmart.com/files/15/Avocado-Half-PNG-Transparent.png'
+          width={600}
+          height={600}>
+        </Image>
+        <button className={'action-button ' + bs.className} onClick={handleClick}>click aqui</button>  
+      </div>
     </main>
   )
 }
